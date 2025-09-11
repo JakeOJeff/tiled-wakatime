0e7b1b90b041d29d2fc395e72689d81c2e240ab1
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-09-11 16:11:02 +0530

    Added basic arguments and a 2 minute debug time for heartbeat checking

--- wakatime.js
+++ wakatime.js
@@ -1,1 +1,27 @@
-tiled.log("Extension Is enabled");
+var WakaTime = {
+    lastObj:  null,
+    lastTimeUsed: 0,
+
+    pushHeartBeat: function(filePath, isWrite) {
+        let currentDate = Date.now()
+
+        // for further tweaks : this is for less than 2 mins, etc
+        if (!isWrite && this.lastObj == filePath && (now - lastTime < 120000)) {
+            return;
+        }
+
+        let args = [
+            "--entity", filePath,
+            // Add plugin later 
+        ]
+
+        if (isWrite) {
+            args.push("--write");
+        }
+
+
+
+        this.lastObj = filePath;
+        this.lastTimeUsed = currentDate;
+    }
+}
